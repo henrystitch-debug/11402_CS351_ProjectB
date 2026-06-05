c83d1ea623a323d31f6d1154ef438766241a984f
henrystitch <henrystitch@gmail.com>
2026-06-05 15:42:21 +0800

    Add gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/olivi/Gitrepo/11402_CS351_ProjectB
-# Build directory: C:/Users/olivi/Gitrepo/11402_CS351_ProjectB/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CSVProcessorTests "C:/Users/olivi/Gitrepo/11402_CS351_ProjectB/build/run_tests.exe")
-set_tests_properties(CSVProcessorTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/olivi/Gitrepo/11402_CS351_ProjectB/CMakeLists.txt;36;add_test;C:/Users/olivi/Gitrepo/11402_CS351_ProjectB/CMakeLists.txt;0;")
